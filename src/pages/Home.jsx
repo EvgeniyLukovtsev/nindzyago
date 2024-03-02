f0a63bdba895502dec6c65dde906c6334beaaae8
Evgeniy Lukovtsev <evgeniylukovtsev@icloud.com>
2024-03-02 20:02:57 +0600

    added createAsyncThunk

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, useContext } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import axios from "axios";
 import qs from "qs";
 import { useNavigate } from "react-router-dom";
 
@@ -16,17 +15,19 @@ import {
   setCurrentPage,
   setFilters,
 } from "../redux/slices/filterSlice";
+import { fetchSneakers } from "../redux/slices/sneakersSlice";
 import { list } from "../components/Sort";
 
 export default function Home() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { items, status } = useSelector((state) => state.sneaker);
   const categoryId = useSelector((state) => state.filter.categoryId);
   const sortType = useSelector((state) => state.filter.sort.sortProperty);
   const currentPage = useSelector((state) => state.filter.currentPage);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
 
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  // const [items, setItems] = useState([]);
+  // const [isLoading, setIsLoading] = useState(true);
   const { searchValue } = useContext(SearchContext);
 
   const onClickCategory = (id) => {
@@ -37,23 +38,18 @@ export default function Home() {
     dispatch(setCurrentPage(number));
   };
 
-  useEffect(() => {
-    setIsLoading(true);
-
+  const getSneakers = async () => {
     const sortBy = sortType.replace("-", "");
     const order = sortType.includes("-") ? "asc" : "desc";
     const category = categoryId > 0 ? `categories=${categoryId}` : "";
 
-    axios
-      .get(
-        `https://65cb6200efec34d9ed8763e5.mockapi.io/items?page=${currentPage}&limit=8&${category}&sortBy=${sortBy}&order=${order}`
-      )
-      .then((response) => {
-        setItems(response.data);
-        setIsLoading(false);
-      });
+    dispatch(fetchSneakers({ sortBy, order, category, currentPage }));
 
     window.scrollTo(0, 0);
+  };
+
+  useEffect(() => {
+    getSneakers();
   }, [categoryId, sortType, currentPage]);
 
   useEffect(() => {
@@ -94,20 +90,31 @@ export default function Home() {
         <Categories value={categoryId} onClickCategory={onClickCategory} />
         <Sort />
       </>
-      <div className={style.container}>
-        {isLoading
-          ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
-          : sneakers.map(({ name, sizes, price, imageUrl, id }) => (
-              <SneakersBlock
-                name={name}
-                sizes={sizes}
-                price={price}
-                imageUrl={imageUrl}
-                key={id}
-                id={id}
-              />
-            ))}
-      </div>
+      {status === "error" ? (
+        <div>
+          <h2>Произошла ошибка</h2>
+          <p>
+            Не удалось получить ответ от сервера. Попробуйте повторить попытку
+            позжею
+          </p>
+        </div>
+      ) : (
+        <div className={style.container}>
+          {status === "loading"
+            ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
+            : sneakers.map(({ name, sizes, price, imageUrl, id }) => (
+                <SneakersBlock
+                  name={name}
+                  sizes={sizes}
+                  price={price}
+                  imageUrl={imageUrl}
+                  key={id}
+                  id={id}
+                />
+              ))}
+        </div>
+      )}
+
       <Pagination currentPage={currentPage} onChangePage={onChangePage} />
     </div>
   );
